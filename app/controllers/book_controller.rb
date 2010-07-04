bbeb18982fa1ae598ace68836c49bf25e0545cb5
Pierre Lasante <pierre.lasante@videotron.ca>
2010-07-04 17:51:21 -0400

    ajax book delete is working

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -6,4 +6,23 @@ class BookController < ApplicationController
     @keyword = params[:book_keyword]
     @title = "Book Shelf Search Results"
   end
+
+  def add
+    isbn = params[:isbn]
+    book = Book.find_or_create_from_amazon( isbn , session[:user] )
+    if book.save
+      render :partial => 'book_exists', :locals => { :book => book }
+    else
+      render :text => 'Failed to add book'
+    end
+  end
+
+  def delete
+    @book = Book.find_by_isbn( params[:isbn] )
+    current_user = User.find( session[:user] )
+    current_user.books.delete( @book )
+    if @book.users.size == 0
+      Book.delete( @book.id )
+    end
+  end
 end
