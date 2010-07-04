6dfd84d453982ae0499ef07db52df1335d4cf05b
Pierre Lasante <pierre.lasante@videotron.ca>
2010-07-04 12:37:22 -0400

    added paging functionalities in search.html.erb

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,6 +1,9 @@
 class BookController < ApplicationController
   def search
+    @prev_page = params[:page].to_i - 1
+    @next_page = params[:page].to_i + 1
     @books = Book.search_amazon(params[:book_keyword], params[:page], session[:user])
+    @keyword = params[:book_keyword]
     @title = "Book Shelf Search Results"
   end
 end
