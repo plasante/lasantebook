c783eed00cf04b0dbbc16253e6bbe43ad8212244
Pierre Lasante <pierre.lasante@videotron.ca>
2010-07-26 13:44:19 -0400

    started implementing views for dvds

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 6) do
+ActiveRecord::Schema.define(:version => 7) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(:version => 6) do
   create_table "books_users", :id => false, :force => true do |t|
     t.integer  "book_id"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "dvds", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
