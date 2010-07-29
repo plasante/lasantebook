9d04a1f2c34a8da5cf0502b8e57a9f5f70c16c39
Pierre Lasante <pierre.lasante@videotron.ca>
2010-07-29 11:50:07 -0400

    Fixed a bug related to dynamic menu value

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,3 +9,30 @@ function show_add_review_form() {
     Element.toggle('add_review');
     Element.toggle('cancel_add_review');
 }
+
+function show_menu_value() {
+	if (window.name.length > 1) {
+		show_menu(window.name);
+	} else {
+		show_menu('Books');
+	}
+}
+
+function show_menu(object) {
+	if (object == 'Books') {
+		$('books_menu').show(); $('books_btn').setStyle({ backgroundColor: '#EEEEEE'});
+		$('music_menu').hide(); $('music_btn').setStyle({ backgroundColor: '#FFFFFF'});
+		$('dvds_menu').hide();  $('dvds_btn').setStyle({ backgroundColor: '#FFFFFF'});
+		window.name = "Books";
+	} else if (object == 'CDs') {
+		$('books_menu').hide(); $('books_btn').setStyle({ backgroundColor: '#FFFFFF'});
+		$('music_menu').show(); $('music_btn').setStyle({ backgroundColor: '#EEEEEE'});
+		$('dvds_menu').hide();  $('dvds_btn').setStyle({  backgroundColor: '#FFFFFF'});
+		window.name = "CDs";	
+	} else {
+		$('books_menu').hide(); $('books_btn').setStyle({ backgroundColor: '#FFFFFF'});
+		$('music_menu').hide(); $('music_btn').setStyle({ backgroundColor: '#FFFFFF'});
+		$('dvds_menu').show();	$('dvds_btn').setStyle({ backgroundColor: '#EEEEEE'});
+		window.name = "DVDs";
+	}
+}
