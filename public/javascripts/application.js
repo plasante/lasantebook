331f71b037a24b3e7e3f28d2f953fd7b5217a524
Pierre Lasante <pierre.lasante@videotron.ca>
2010-07-30 13:20:37 -0400

    added Home button

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -19,17 +19,26 @@ function show_menu_value() {
 }
 
 function show_menu(object) {
-	if (object == 'Books') {
+	if (object == 'Home') {
+		$('home_menu').show();  $('home_btn').setStyle({ backgroundColor: '#EEEEEE'});
+		$('books_menu').show(); $('books_btn').setStyle({ backgroundColor: '#FFFFFF'});
+		$('music_menu').hide(); $('music_btn').setStyle({ backgroundColor: '#FFFFFF'});
+		$('dvds_menu').hide();  $('dvds_btn').setStyle({ backgroundColor: '#FFFFFF'});
+		window.name = "Home";		
+	}else if (object == 'Books') {
+		$('home_menu').show();  $('home_btn').setStyle({ backgroundColor: '#FFFFFF'});
 		$('books_menu').show(); $('books_btn').setStyle({ backgroundColor: '#EEEEEE'});
 		$('music_menu').hide(); $('music_btn').setStyle({ backgroundColor: '#FFFFFF'});
 		$('dvds_menu').hide();  $('dvds_btn').setStyle({ backgroundColor: '#FFFFFF'});
 		window.name = "Books";
 	} else if (object == 'CDs') {
+		$('home_menu').show();  $('home_btn').setStyle({ backgroundColor: '#FFFFFF'});
 		$('books_menu').hide(); $('books_btn').setStyle({ backgroundColor: '#FFFFFF'});
 		$('music_menu').show(); $('music_btn').setStyle({ backgroundColor: '#EEEEEE'});
 		$('dvds_menu').hide();  $('dvds_btn').setStyle({  backgroundColor: '#FFFFFF'});
 		window.name = "CDs";	
 	} else {
+		$('home_menu').show();  $('home_btn').setStyle({ backgroundColor: '#FFFFFF'});
 		$('books_menu').hide(); $('books_btn').setStyle({ backgroundColor: '#FFFFFF'});
 		$('music_menu').hide(); $('music_btn').setStyle({ backgroundColor: '#FFFFFF'});
 		$('dvds_menu').show();	$('dvds_btn').setStyle({ backgroundColor: '#EEEEEE'});
